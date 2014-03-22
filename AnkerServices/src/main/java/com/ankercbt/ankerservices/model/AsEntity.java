160baf3e8114d91a65de129acbf773d8b1ae273d
Tobias Schneck <tobias.schneck@consol.de>
2014-03-22 13:51:18 +0100

    uncomment consol proxy

--- AnkerServices/src/main/java/com/ankercbt/ankerservices/model/AsEntity.java
+++ AnkerServices/src/main/java/com/ankercbt/ankerservices/model/AsEntity.java
@@ -1,5 +1,6 @@
 package com.ankercbt.ankerservices.model;
 
+import com.sun.istack.internal.Nullable;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -18,16 +19,39 @@ public abstract class AsEntity implements Comparable<AsEntity> {
     @Id
     private BigInteger id;
 
+    public BigInteger getId() {
+        return id;
+    }
+
     @Override
-    public int compareTo(AsEntity o) {
+    public int compareTo(@Nullable AsEntity o) {
+        if (o == null) {
+            return (id == null) ? 0 : 1;
+        }
+        if (id == null) {
+            return (o.id == null) ? 0 : 1;
+
+        }
         return this.id.compareTo(o.id);
     }
 
     @Override
     public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (this.id == null || o == null) {
+            return false;
+        }
+
         if (o instanceof AsEntity) {
-            return compareTo((AsEntity) o) == 0;
+            return this.id.equals(((AsEntity) o).id);
         }
         return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id.hashCode();
     }
 }
